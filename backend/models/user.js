80605530a88095777ae94d24223e04a88f2fa7d2
robin hunter <rbnrggs@yahoo.com>
2023-11-13 10:34:57 -1000

    save

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,9 +4,9 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
 	email: { type: String, unique: true, required: true },
 	password: { type: String, minlength: 6, required: true },
-  note: { type: [String], },
-  favoriteLocation: { type: [String], },
-  image: { type: [String], },
+  note: String,
+  favoriteLocation: String,
+  image: String,
 })
 
 module.exports = mongoose.model('User', UserSchema)
